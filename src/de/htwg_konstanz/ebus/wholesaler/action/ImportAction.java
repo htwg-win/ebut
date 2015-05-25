07a5366c90e86e212edc6db5556d422cc8162193
Johannes Dangelmaier <e.mail-adresse@gmx.de>
2015-05-25 10:56:41 +0200

    adding import/export buttons and jsp's

---
+++ src/de/htwg_konstanz/ebus/wholesaler/action/ImportAction.java
@@ -0,0 +1,49 @@
+package de.htwg_konstanz.ebus.wholesaler.action;
+
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import de.htwg_konstanz.ebus.wholesaler.demo.ControllerServlet;
+import de.htwg_konstanz.ebus.wholesaler.demo.IAction;
+import de.htwg_konstanz.ebus.wholesaler.demo.util.Constants;
+
+/**
+* The ImportAction processes an import request.<p>
+*
+* @author jd
+*/
+public class ImportAction implements IAction
+{
+	public ImportAction()
+	{
+		super();
+	}
+
+   /**
+   * The execute method is automatically called by the dispatching sequence of the {@link ControllerServlet}. 
+   * 
+   * @param request the HttpServletRequest-Object provided by the servlet engine
+   * @param response the HttpServletResponse-Object provided by the servlet engine
+   * @param errorList a Stringlist for possible error messages occured in the corresponding action
+   * @return the redirection URL
+   */
+	public String execute(HttpServletRequest request, HttpServletResponse response, ArrayList<String> errorList)
+	{
+		return "import.jsp";
+	}
+
+   /**
+   * Each action itself decides if it is responsible to process the corrensponding request or not.
+   * This means that the {@link ControllerServlet} will ask each action by calling this method if it
+   * is able to process the incoming action request, or not.
+   * 
+   * @param actionName the name of the incoming action which should be processed
+   * @return true if the action is responsible, else false
+   */
+	public boolean accepts(String actionName)
+	{
+		return actionName.equalsIgnoreCase(Constants.ACTION_SHOW_IMPORT);
+	}
+}
--- src/de/htwg_konstanz/ebus/wholesaler/demo/util/Constants.java
+++ src/de/htwg_konstanz/ebus/wholesaler/demo/util/Constants.java
@@ -63,4 +63,8 @@ public class Constants
 	public static final int USER_INTERNAL = 1;
 	public static final int USER_CUSTOMER = 2;
 	public static final int USER_SUPPLIER = 3;
+	
+	
+	public static final String ACTION_SHOW_IMPORT = "showImport";
+	public static final String ACTION_SHOW_EXPORT = "showExport";
 }
